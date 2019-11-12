fe93dcb0a6ec26c0fd2a8f83ab14c0be9ebc85d5
Crystian Yamakawa <crystian_yamakawa@hotmail.com>
2019-11-11 21:02:11 -0300

    5-Extract Method

--- src/locadora/Customer.java
+++ src/locadora/Customer.java
@@ -28,10 +28,7 @@ class Customer {
 
 
 	        // add frequent renter points
-	        frequentRenterPoints ++;
-	        // add bonus for a two day new release rental
-	        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-	            each.getDaysRented() > 1) frequentRenterPoints ++;
+	        frequentRenterPoints += each.getFrequentRenterPoints();
 
 	        //show figures for this rental
 	        result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -45,4 +42,6 @@ class Customer {
 	             " frequent renter points";
 	     return result;
 	}
+
+	
 }
--- src/locadora/Rental.java
+++ src/locadora/Rental.java
@@ -34,4 +34,12 @@ class Rental {
 		}
 		return result;
 	}
+	 int getFrequentRenterPoints() {
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+		    getDaysRented() > 1) 
+			return 2;
+		else
+			return 1;
+	}
 }
